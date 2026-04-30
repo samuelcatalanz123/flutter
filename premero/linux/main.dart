ad0b8029321338efdabbb49957149262ebcc1941
samuel catalan <samuelcatalanz123@gmail.comalanz123@gmail.com>
2026-04-30 12:22:29 -0600

    primer commit,

---
+++ premero/linux/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hello_worlrd_app/presentation/screens/counter/counter_screens.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CounterScreen(),
+    ); // MaterialApp
+  }
+}
